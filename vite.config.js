9617a8160bdda32a3c6b90f6b68232581271a9c9
Jeet Mehta <jeetmehta@Jeets-MacBook-Air.local>
2026-05-08 16:51:10 -0400

    Remove deployment throiugh gh Actions

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // For GitHub Pages you may later want to set base: "/<repo>/"
-  // We keep it "/" for local dev and custom domains.
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
+  },
   server: {
     host: "127.0.0.1",
     port: 5173,
