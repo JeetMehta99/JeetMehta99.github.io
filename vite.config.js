ceb756d006385d5c8a31ed0db9ce03a8f7769490
Jeet Mehta <jeetmehta@Jeets-MacBook-Air.local>
2026-05-08 16:56:29 -0400

    Build site for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "app",
   plugins: [react()],
+  publicDir: "../public",
   build: {
-    outDir: "docs",
+    outDir: "../docs",
     emptyOutDir: true
   },
   server: {
